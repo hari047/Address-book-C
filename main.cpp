2ba89e4b5c6e6e0255e3332c052c92920c6d448e
Hari Prasad Rangaraj <40194788+hari047@users.noreply.github.com>
2026-06-25 09:22:13 +0930

    Add functions to modify person name and find most searched restaurant

--- main.cpp
+++ main.cpp
@@ -270,6 +270,107 @@ public:
 
 };
 
+void modifyPersonName()
+{
+    int found=0;
+    char NAME[30];
+    cout<<"\nEnter name to modify : ";
+    cin>>NAME;
+    system("cls");
+    pfp.open("Person.txt",ios::in|ios::out);
+    while(pfp.read((char*)&p,sizeof(Person))&&found==0)
+    {
+     if(strcmpi(NAME,p.returnName())==0)
+        {
+            system("cls");
+            p.show();
+            cout<<"\n\tEnter new details"<<endl;
+            p.getData();
+            int pos=-1*sizeof(p);
+			pfp.seekp(pos,ios::cur);
+            pfp.write((char*)&p,sizeof(Person));
+            cout<<"\n\tRecord update";
+            found=1;
+        }
+    }
+    pfp.close();
+    if(found==0)
+        cout<<"\n\tRecord not found";
+    getch();
+}
+
+void mostSearchedRestaurant()
+{
+    int i,j;
+    Restaurant popular,temp;
+    int tempCount,cnt=1;
+    popular=r_arr[0];
+    for(i=0 ; i<nr ; i++)
+    {
+        tempCount=0;
+        temp=r_arr[i];
+        tempCount++;
+        for(j=i+1 ; j<nr ; j++)
+        {
+            if(strcmpi(r_arr[j].returnName(),temp.returnName())==0)
+            {
+                tempCount++;
+                if(tempCount > cnt)
+                {
+                    popular=temp;
+                    cnt=tempCount;
+                }
+            }
+        }
+    }
+    if(nr==0)
+        cout<<"\n\tNo restaurants searched.";
+    else
+    {
+        cout<<"\n\tMOST SEARCHED RESTAURANT";
+        popular.show();
+    }
+    getch();
+}
+
+int main()
+{
+    int ch;
+    while(1)
+    {
+        system("cls");
+        system("color 02");
+        cout<<"\n\t\tMAIN MENU\n";
+        cout<<"\n\t1. CREATE\n\t2. SEARCH\n\t3. DISPLAY\n\t4. MODIFY\n\t5. MOST SEARCHED\n\t6. DELETE\n\t7. EXIT\n\n\tEnter choice : ";
+        cin>>ch;
+        switch(ch)
+        {
+            case 1: create();
+                    break;
+
+            case 2: ssearch();
+                    break;
+
+            case 3: display();
+                    break;
+
+            case 4: modify();
+                    break;
+
+            case 5: mostSearched();
+                    break;
+
+            case 6: deleteRecords();
+                    break;
+
+            case 7: exit(0);
+
+            default: cout<<"\n\tINVALID CHOICE\n";
+                     break;
+        }
+    }
+
+}
 
 // This is just a sample of the source code to the project.
 // The original project has 1500 lines of code with hospital, Mall and Restaurants
